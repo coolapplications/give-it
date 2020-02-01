29c10095630e4904aa8891b16a2cb77f0be36ebb
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2020-01-31 22:36:07 -0500

    show 0 in the badge

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -1,16 +1,11 @@
-import React from "react";
-import SearchBar from "../SearchBar/SearchBar";
-import ProductList from "../ProductList/ProductList";
-import { RootState } from "../../store";
-import { useSelector } from "react-redux";
+import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import ProductList from '../ProductList/ProductList';
 
 export default function Home() {
-  const counterAdd = useSelector(
-    (store: RootState) => store.myProductListReducer.result.length
-  );
   return (
     <div>
-      <SearchBar counter={counterAdd} />
+      <SearchBar />
       <ProductList />
     </div>
   );
--- src/components/Pages/MyGifts.tsx
+++ src/components/Pages/MyGifts.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import MyList from "../MyList/MyList";
 import SearchBar from "../SearchBar/SearchBar";
-import { RootState } from "../../store";
-import { useSelector } from "react-redux";
+
 
 export default function List() {
-  const counterRemove = useSelector(
-    (store: RootState) => store.myProductListReducer.result.length
-  );
+  
   return (
     <div>
-      <SearchBar counter={counterRemove} />
+      <SearchBar  />
       <MyList />
     </div>
   );
