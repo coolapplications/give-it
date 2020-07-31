99ee07c698383673d9031e4cbdaa5754c2041a65
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2020-07-31 16:26:42 -0500

    auth0 config remove

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,26 +1,10 @@
-import React from "react";
-import { useAuth0 } from "../../react-auth0-spa";
+import React from 'react';
 
 export default function Login() {
-  const auth0Context = useAuth0();
-
   return (
     <div>
-      {!auth0Context?.isAuthenticated && (
-        <button onClick={() => auth0Context?.loginWithRedirect({})}>
-          Log in
-        </button>
-      )}
-
-      {auth0Context?.isAuthenticated && (
-        <button
-          onClick={() =>
-            auth0Context?.logout({ returnTo: `${window.location.origin}/home` })
-          }
-        >
-          Log out
-        </button>
-      )}
+      <button>Log in</button>
+      <button>Log out</button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,44 +5,22 @@ import MyGifts from './components/Pages/MyGifts';
 import Home from './components/Pages/Home';
 import store from './store';
 import { Provider } from 'react-redux';
-import history from './history';
-import { Auth0Provider } from './react-auth0-spa';
-import config from './auth_config.json';
 import LoginRedirect from './components/LoginRedirect/LoginRedirect';
 import 'bootstrap/dist/css/bootstrap.css';
 
-// A function that routes the user to the right place
-// after login
-const onRedirectCallback = (appState: any) => {
-  history.push(
-    appState && appState.targetUrl
-      ? `${appState.targetUrl}/login`
-      : `${window.location.origin}/login`
-  );
-};
-
 const Root = (
-  <Auth0Provider
-    initOptions={{
-      domain: config.domain,
-      client_id: config.clientId,
-      redirect_uri: `${window.location.origin}/login`
-    }}
-    onRedirectCallback={onRedirectCallback}
-  >
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route path='/home' component={Home} />
-          <Route path='/gifts' component={MyGifts} />
-          <Route path='/login' component={LoginRedirect} />
-          <Route exact path='/'>
-            <Redirect to='/home' />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </Provider>
-  </Auth0Provider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/home' component={Home} />
+        <Route path='/gifts' component={MyGifts} />
+        <Route path='/login' component={LoginRedirect} />
+        <Route exact path='/'>
+          <Redirect to='/home' />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(Root, document.getElementById('root'));
